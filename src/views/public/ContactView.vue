6efd6ebadf06ac7e8b1bba0b18a3962b7ada529e
Ahnaf Anan <aanana0171@gmail.com>
2026-05-16 22:55:38 +0600

    ContactView.vue file developed

---
+++ src/views/public/ContactView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+import ContactHero from '../../components/contact/ContactHero.vue'
+import ContactGridSection from '../../components/contact/ContactGridSection.vue'
+import FaqSection from '../../components/contact/FaqSection.vue'
+import ContactSuccessToast from '../../components/contact/ContactSuccessToast.vue'
+
+const toastVisible = ref(false)
+
+function handleContactSubmit(formData) {
+    console.log('Contact submitted:', formData)
+    toastVisible.value = true
+
+    setTimeout(() => {
+        toastVisible.value = false
+    }, 3000)
+}
+</script>
+
+<template>
+    <div>
+        <ContactHero />
+        <ContactGridSection @submitted="handleContactSubmit" />
+        <FaqSection />
+        <ContactSuccessToast :visible="toastVisible" />
+    </div>
+</template>

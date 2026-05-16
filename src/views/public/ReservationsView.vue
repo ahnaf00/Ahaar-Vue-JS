431f850ac507e224abb7a1a63d1ef9f976f29c7f
Ahnaf Anan <aanana0171@gmail.com>
2026-05-16 22:22:35 +0600

    ReservationsView.vue file developed

---
+++ src/views/public/ReservationsView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+import ReservationHero from '../../components/reservations/ReservationHero.vue'
+import ReservationSuccessToast from '../../components/reservations/ReservationSuccessToast.vue'
+
+const toastVisible = ref(false)
+
+function handleReservationSubmit(formData) {
+    console.log('Reservation submitted:', formData)
+    toastVisible.value = true
+
+    setTimeout(() => {
+        toastVisible.value = false
+    }, 3000)
+}
+</script>
+
+<template>
+    <div>
+        <ReservationHero @submitted="handleReservationSubmit" />
+        <ReservationSuccessToast :visible="toastVisible" />
+    </div>
+</template>

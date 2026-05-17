eaf482bbdad647c65be563f76da068eb8f2c557a
Ahnaf Anan <aanana0171@gmail.com>
2026-05-17 21:50:21 +0600

    guards.js file created

---
+++ src/stores/guards.js
@@ -0,0 +1,13 @@
+import { useAuthStore } from "./auth"; 
+
+export function requiresAuth(to,from,next)
+{
+    const auth = useAuthStore()
+    if(to.meta.requiresAuth && !auth.isAuthenticated)
+    {
+        next({name:'AdminLogin',query:{redirect:to.fullpath}})
+    }else
+    {
+        next()
+    }
+}
